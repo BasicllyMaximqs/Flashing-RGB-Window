aa6086eb6aa8bfc92a3c49164999771d494d3454
mex <65974823+BasicllyMaximqs@users.noreply.github.com>
2020-10-12 16:13:05 +0200

    Add files via upload

---
+++ RGBLighting/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGBLighting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_LoadAsync(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+            int delay = 150;
+
+            async void aAsync()
+            {
+                await Task.Delay(delay);
+                this.BackColor = Color.Blue;
+                await Task.Delay(delay);
+                this.BackColor = Color.Red;
+                await Task.Delay(delay);
+                this.BackColor = Color.Yellow;
+                await Task.Delay(delay);
+                this.BackColor = Color.LimeGreen;
+                aAsync();
+            }
+
+            aAsync();
+        }
+
+    }
+}
